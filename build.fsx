53d63efe1ead88493f2a56e9873b18ae954805b8
Simon Dickson <simonhdickson@gmail.com>
2013-12-20 15:14:18 +0000

    Adds nuget building

--- build.fsx
+++ build.fsx
@@ -6,6 +6,14 @@ RestorePackages()
 let buildDir = "./build/"
 let testDir  = "./test/"
 
+let project = "Fanciful"
+let authors = ["Simon Dickson"]
+let summary = "A more strongly typed way to use Nancy"
+let description = """
+  An F# friendly wrapper around Nancy, alose aiming for compatibile with Nancy"""
+
+let tags = "F# fsharp nancy fancy fanciful"
+
 Target "Clean" (fun _ ->
     CleanDirs [buildDir; testDir]
 )
@@ -28,6 +36,25 @@ Target "Test" (fun _ ->
           {p with
              ShadowCopy = false;
              OutputDir = testDir})
+)
+
+Target "NuGet" (fun _ ->
+    let description = description.Replace("\r", "").Replace("\n", "").Replace("  ", " ")
+    let nugetPath = ".nuget/nuget.exe"
+    NuGet (fun p -> 
+        { p with   
+            Authors = authors
+            Project = project
+            Summary = summary
+            Description = description
+            Version = "0.1"
+            Tags = tags
+            OutputPath = "bin"
+            ToolPath = nugetPath
+            AccessKey = getBuildParamOrDefault "nugetkey" ""
+            Publish = hasBuildParam "nugetkey"
+            Dependencies = [] })
+        "/FSharp.Formatting.nuspec"
 )
 
 "Clean"
