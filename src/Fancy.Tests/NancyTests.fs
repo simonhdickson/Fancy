5df3b4888e468fb8b66f9e1e21dcbd54b1d25175
Albert-Jan Nijburg <albertjan@curit.com>
2014-02-03 11:24:25 +0100

    Fixed tests

--- src/Fancy.Tests/NancyTests.fs
+++ src/Fancy.Tests/NancyTests.fs
@@ -3,20 +3,22 @@ open Fancy
 open Xunit
 open Xunit.Extensions
 
+type Alpha = Alpha of string 
+
 module NancyTests =
     let ``example urls`` : obj[] seq =
         seq {
-            yield [| "/"; [|typeof<obj>|]; "/" |]    
-            yield [|"/HelloWorld"; [|typeof<obj>|]; "/HelloWorld"|] 
-            yield [|"/%s"; [|typeof<string>|]; "/{%s}"|]
-            yield [|"/%i"; [|typeof<int>|]; "/{%s:int}"|]   
-            yield [|"/%d"; [|typeof<decimal>|]; "/{%s:decimal}"|]
-            yield [|"/%b"; [|typeof<bool>|]; "/{%s:bool}"|]
+            yield [| "/"; [("", typeof<Unit>)]; "/" |]    
+            yield [|"/HelloWorld"; [("", typeof<Unit>)]; "/HelloWorld"|] 
+            yield [|"/%s"; [("bananas", typeof<string>)]; "/{bananas}"|]
+            yield [|"/%i"; [("monkees", typeof<int>)]; "/{monkees:int}"|]   
+            yield [|"/%d"; [("pi", typeof<decimal>)]; "/{pi:decimal}"|]
+            yield [|"/%b"; [("yes", typeof<bool>)]; "/{yes:bool}"|]
 
-            yield [|"/%s/%s"; [|typeof<string>; typeof<string>|]; "/{%s}/{%s}"|]
-            yield [|"/%s/HelloWorld/%s"; [|typeof<string>; typeof<string>|]; "/{%s}/HelloWorld/{%s}"|]
-            yield [|"/%d/%i"; [|typeof<decimal>; typeof<int>|]; "/{%s:decimal}/{%s:int}"|]
-            yield [|"/%d/HelloWorld/%i"; [|typeof<decimal>; typeof<int>|]; "/{%s:decimal}/HelloWorld/{%s:int}"|]    
+            yield [|"/%s/%s"; [("banana", typeof<string>); ("peel",typeof<string>)]; "/{banana}/{peel}"|]
+            yield [|"/%s/HelloWorld/%s"; [("is", typeof<string>); ("cool", typeof<string>)]; "/{is}/HelloWorld/{cool}"|]
+            yield [|"/%d/%i"; [("pi", typeof<decimal>); ("atoms", typeof<int>)]; "/{pi:decimal}/{atoms:int}"|]
+            yield [|"/%d/HelloWorld/%i"; [("pi", typeof<decimal>); ("atoms", typeof<int>)]; "/{pi:decimal}/HelloWorld/{atoms:int}"|]    
             
             //yield [|"/%A"; [|typeof<Alpha>|]; "/{%s:alpha}"|]
         }
