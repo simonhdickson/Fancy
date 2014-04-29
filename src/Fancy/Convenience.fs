ad1d235df6758e0fa15b06d749aa2373e51d8fc5
Albert-Jan Nijburg <albertjan@curit.com>
2014-04-29 15:49:47 +0200

    Some more stuff

--- src/Fancy/Convenience.fs
+++ src/Fancy/Convenience.fs
@@ -5,6 +5,7 @@ module Convenience
     open System.IO
 
     //Convenience methods for response
+
     ///add a delegate at the end of the list of stream editors
     let addToContents editor (response:Response) = 
         response.Contents <- response.Contents ++ Action<Stream>(editor)
@@ -15,24 +16,24 @@ module Convenience
         response.Contents <- Action<Stream>(editor)
         response
 
-    ///add a list of headers to the response
-    let addHeaders (headers: (string * string) list) (response:Response) = 
-        headers
-        |> Seq.iter (fun h -> match h with | (key, value) -> response.Headers.Add(key, value))
-        response
-
     ///add a header to the reponse
     let addHeader headers (response:Response) = 
          match headers with 
          | (key, value) -> response.Headers.Add(key, value)
          response
-    
+
+    ///add a list of headers to the response
+    let addHeaders headers response = 
+        headers
+        |> Seq.iter (fun h -> addHeader h response |> ignore)
+        response
+
     ///adds a cookie
     let addCookie name value (response:Response) = 
         response.WithCookie(name, value) 
 
     ///adds a cookie with expiration
-    let addCookieWithExpr name value (expires:DateTime) (response:Response) = 
+    let addCookieWithExpr name value expires (response:Response) = 
         let exp = Nullable expires 
         response.WithCookie(name, value, exp) 
 
