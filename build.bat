19593134f44ff2f80eff12aab55225783b13837b
Albert-Jan Nijburg <albertjan@curit.com>
2014-09-17 09:00:37 +0200

    updates fake file to make fancy.testing package

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 @echo off
 if not exist packages\FAKE\tools\Fake.exe ( 
-  .nuget\nuget.exe install FAKE -OutputDirectory packages -Prerelease -ExcludeVersion  
+  .nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion  
 )
 if not exist packages\FSharp.Formatting\lib\net40\FSharp.Literate.dll ( 
-  .nuget\nuget.exe install FSharp.Formatting -OutputDirectory packages -Version 2.0.2 -ExcludeVersion  
+  .nuget\nuget.exe install FSharp.Formatting -OutputDirectory packages -ExcludeVersion  
 )
 packages\FAKE\tools\FAKE.exe build.fsx %*
 pause
